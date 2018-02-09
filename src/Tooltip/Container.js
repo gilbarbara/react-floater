637d0f9813a5803f3e7f15f7562c7d4eece00f47
Gil Barbara <gilbarbara@gmail.com>
2018-02-08 23:33:37 -0200

    split Tooltip into smaller components
    - add hideArrow prop

---
+++ src/Tooltip/Container.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CloseBtn from './CloseBtn';
+
+const TooltipContainer = ({
+  content,
+  footer,
+  handleClick,
+  open,
+  positionWrapper,
+  showCloseButton,
+  title,
+  styles,
+}) => {
+  const output = {
+    content: React.isValidElement(content)
+      ? content
+      : <div className="__tooltip__content" style={styles.content}>{content}</div>
+  };
+
+  if (title) {
+    output.title = React.isValidElement(title)
+      ? title
+      : <div className="__tooltip__title" style={styles.title}>{title}</div>;
+  }
+
+  if (footer) {
+    output.footer = React.isValidElement(footer)
+      ? footer
+      : <div className="__tooltip__footer" style={styles.footer}>{footer}</div>;
+  }
+
+  if (
+    (showCloseButton || positionWrapper)
+    && typeof open === 'undefined'
+  ) {
+    output.close = (
+      <CloseBtn
+        styles={styles.close}
+        handleClick={handleClick}
+      />
+    );
+  }
+  return (
+    <div className="__tooltip__container" style={styles.container}>
+      {output.close}
+      {output.title}
+      {output.content}
+      {output.footer}
+    </div>
+  );
+};
+
+TooltipContainer.propTypes = {
+  content: PropTypes.node.isRequired,
+  footer: PropTypes.node,
+  handleClick: PropTypes.func.isRequired,
+  open: PropTypes.bool,
+  positionWrapper: PropTypes.bool.isRequired,
+  showCloseButton: PropTypes.bool.isRequired,
+  styles: PropTypes.object.isRequired,
+  title: PropTypes.node,
+};
+
+export default TooltipContainer;
