9152244acfb7bd0f920c2ca9f7b5be4cb98277ea
Gil Barbara <gilbarbara@gmail.com>
2019-01-25 19:12:37 -0200

    Update build setup

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,22 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import packageJSON from './package.json';
 
+const plugins = () => [
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  commonjs(),
+];
+
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'es/index.js',
-    format: 'es',
-  },
+  output: [
+    { file: 'lib/index.js', format: 'cjs' },
+    { file: 'es/index.js', format: 'es' },
+  ],
   external: [
     ...Object.keys(packageJSON.peerDependencies),
     ...Object.keys(packageJSON.dependencies),
   ],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    commonjs(),
-  ],
+  plugins: [...plugins()],
 };
