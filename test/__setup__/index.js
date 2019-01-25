03a3d63378518bbf465125f9ad0cf4d53106bbfa
Gil Barbara <gilbarbara@gmail.com>
2019-01-25 19:11:59 -0200

    Update jest setup

--- test/__setup__/index.js
+++
@@ -1,9 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-const react = document.createElement('div');
-react.id = 'react';
-react.style.height = '100vh';
-document.body.appendChild(react);
